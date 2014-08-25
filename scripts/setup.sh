ba5b55111f507815e6d0626675b0eed216583f55
Kristoffer Walker <kris@kixx.name>
2014-08-25 13:23:42 -0400

    Restructure script for functional utility.
     On branch master
    	modified:   scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,86 +1,115 @@
 #!/usr/bin/env bash
-echo "************************************************************"
-echo "Updating packages..."
-echo "************************************************************"
-sudo apt-get update
 
-echo "************************************************************"
-echo "Installing apt-add-repository"
-echo "************************************************************"
-sudo apt-get install -y python-software-properties
+fail () {
+  echo "$@" >&2
+  exit 1
+}
 
-echo "************************************************************"
-echo "Installing good-to-have packages"
-echo "************************************************************"
-sudo apt-get install -y build-essential git-core vim curl ack-grep wget tree
+main () {
+  update_package_list
+  install_apt_add
+  install_tools
+  add_nginx_repo
+  update_package_list
+  install_nginx
+  install_php5_fpm
+  clean_up_packages
+  install_bash_profile
+  configure_php5fpm
+  configure_nginx
+  restart_services
+}
 
-echo "************************************************************"
-echo "Adding ppa:nginx/stable"
-echo "************************************************************"
-sudo apt-add-repository -y ppa:nginx/stable
+print_section () {
+  local msg="$1"
+  echo "************************************************************"
+  echo "$msg"
+  echo "************************************************************"
+}
 
-echo "************************************************************"
-echo "Updating again"
-echo "************************************************************"
-sudo apt-get update
+install_tools () {
+  print_section "Installing good-to-have packages"
+  sudo apt-get install -y \
+    build-essential \
+    git-core \
+    vim \
+    curl \
+    ack-grep \
+    wget \
+    tree \
+    || fail "Unable to install tools."
+}
 
-echo "************************************************************"
-echo "Installing nginx"
-echo "************************************************************"
-sudo apt-get install -y nginx
+install_apt_add () {
+  print_section "Installing apt-add-repository"
+  sudo apt-get install -y python-software-properties || fail "Unable to install python-software-properties"
+}
 
-echo "************************************************************"
-echo "Installing PHP5 FPM"
-echo "************************************************************"
-sudo apt-get install -y \
-  php5 \
-  php5-fpm \
-  php5-common \
-  php5-dev \
-  php5-gd \
-  php5-xcache \
-  php5-mcrypt \
-  php5-pspell \
-  php5-snmp \
-  php5-xsl \
-  php5-imap \
-  php5-geoip \
-  php5-curl \
-  php5-cli
+add_nginx_repo () {
+  print_section "Adding ppa:nginx/stable"
+  sudo apt-add-repository -y ppa:nginx/stable || fail "Unable to install Nginx repo"
+}
 
-if [ ! -d /var/run/php5-fpm ]; then
-  sudo mkdir /var/run/php5-fpm
-fi
+update_package_list () {
+  print_section "Updating package list"
+}
 
-echo "************************************************************"
-echo "Cleaning up ..."
-echo "************************************************************"
-sudo apt-get autoremove
-sudo apt-get autoclean
+install_nginx () {
+  print_section "Installing nginx"
+  sudo apt-get install -y nginx || fail "Unable to install Nginx"
+}
 
-echo "************************************************************"
-echo "Installing .bash_profile"
-echo "************************************************************"
-cp /vagrant/configs/.bash_profile /home/vagrant/.bash_profile
-chown vagrant:vagrant /home/vagrant/.bash_profile
+install_php5_fpm () {
+  print_section "Installing PHP5 FPM"
+  sudo apt-get install -y \
+    php5 \
+    php5-fpm \
+    php5-common \
+    php5-dev \
+    php5-gd \
+    php5-xcache \
+    php5-mcrypt \
+    php5-pspell \
+    php5-snmp \
+    php5-xsl \
+    php5-imap \
+    php5-geoip \
+    php5-curl \
+    php5-cli \
+    || fail "Unable to install PHP5 FPM"
+}
 
-echo "************************************************************"
-echo "Configuring PHP5 FPM"
-echo "************************************************************"
-sudo cp /vagrant/configs/default_nginx.conf /etc/php5/fpm/pool.d/default_nginx.conf
-sudo cp /vagrant/configs/cmisutah_com.conf /etc/php5/fpm/pool.d/cmisutah_com.conf
-sudo cp /vagrant/configs/index.php /usr/share/nginx/html/index.php
+clean_up_packages () {
+  print_section "Cleaning up packages"
+  (sudo apt-get autoremove -y && sudo apt-get autoclean -y) || fail "Unable to clean up packages"
+}
 
-echo "************************************************************"
-echo "Configuring Nginx site"
-echo "************************************************************"
-sudo cp /vagrant/configs/nginx_default /etc/nginx/sites-available/default
-sudo cp /vagrant/configs/cmisutah_com /etc/nginx/sites-enabled/cmisutah_com
-sudo cp /vagrant/configs/nginx.conf /etc/nginx/nginx.conf
+install_bash_profile () {
+  print_section "Installing .bash_profile"
+  cp /vagrant/configs/.bash_profile /home/vagrant/.bash_profile
+  chown vagrant:vagrant /home/vagrant/.bash_profile
+}
 
-echo "************************************************************"
-echo "Restarting services ..."
-echo "************************************************************"
-sudo service php5-fpm restart
-sudo service nginx restart
+configure_php5fpm () {
+  print_section "Configure PHP5 FPM"
+  if [ ! -d /var/run/php5-fpm ]; then
+    sudo mkdir /var/run/php5-fpm || fail "Unable to create /var/run/php5-fpm"
+  fi
+  (sudo cp /vagrant/configs/default_nginx.conf /etc/php5/fpm/pool.d/default_nginx.conf
+  && sudo cp /vagrant/configs/index.php /usr/share/nginx/html/index.php) || fail "Unable to copy PHP FPM config files."
+}
+
+configure_nginx () {
+  print_section "Configure Nginx"
+  (sudo cp /vagrant/configs/nginx.conf /etc/nginx/nginx.conf
+  && sudo cp /vagrant/configs/nginx_default /etc/nginx/sites-available/default) || fail "Unable to copy Nginx config files."
+}
+
+restart_services () {
+  print_section "Restart services"
+  sudo service php5-fpm restart
+  sudo service nginx restart
+}
+
+main "$@"
 
